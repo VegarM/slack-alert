ef7a634538e8198ba8df9d2b6ee455de38a73b3a
Vegar Sechmann Molvig <vegar.molvig@gmail.com>
2019-01-18 00:47:32 +0100

    Check entire msg for firing.
    time.sleep moved to correct indent

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import time
 import sys
 from slackclient import SlackClient
 
-AUDIO_FILE = os.paht.join(
+AUDIO_FILE = os.path.join(
     'audio', 'Tri-Tachyon_-_04_-_Hundred_Years_in_Helheim.mp3')
 ALERT_CHANNEL = 'CAYH1GKM4'
+#ALERT_CHANNEL = 'D9H0NM0A1'
 
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
@@ -14,7 +15,7 @@ sc = SlackClient(slack_token)
 def handle_msg(msg):
     print(msg)
     if (msg['type'] == 'message' and msg['channel'] == ALERT_CHANNEL and
-            'FIRING' in msg['text']):
+            'firing' in str(msg).lower()):
         os.system('mpv --loop-file {}'.format(AUDIO_FILE))
 
 
@@ -25,7 +26,7 @@ def main():
                 while sc.server.connected:
                     for msg in sc.rtm_read():
                         handle_msg(msg)
-                        time.sleep(1)
+                    time.sleep(1)
             else:
                 print("connection failed")
         except KeyboardInterrupt:
